1da28d7476c3199445d86c93b16460c9a28fb480
Artur2 <a.nurullin@artsofte.ru>
2020-04-07 21:56:57 +0500

    Remove redundant region names

--- src/CarsRental.Crosscutting/RegionNames.cs
+++ src/CarsRental.Crosscutting/RegionNames.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CarsRental.Crosscutting
+﻿namespace CarsRental.Crosscutting
 {
     /// <summary>
     /// Region names related to combining views by Prism.
     /// </summary>
     public class RegionNames
     {
-        public const string HeaderContent = "HeaderContent";
         public const string MainContent = "MainContent";
-        public const string FooterContent = "FooterContent";
     }
 }
